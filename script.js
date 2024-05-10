bffa854e1b98afbd6e7202290faeb34e1e9df9b8
cperalt <cperalt@github.com>
2024-05-10 10:42:35 -0400

    Created Sorting Function

--- script.js
+++ script.js
@@ -7,14 +7,28 @@ async function fetchData() {
             throw new Error('Failed to fetch data');
         }
         const data = await response.json()
+
+        sortData(data);
         displayData(data);
     } catch (error) {
         console.error('Error:', error);
     }
 };
 
+const sortData = (data) => {
+    return data.sort((a, b) => {
+        if (a.color == 'white') {
+            return 1;
+        }
+        if (a.color == 'black') {
+            return -1;
+        }
+    });
+};
+
 const displayData = (data) => {
     data.forEach((obj) => {
+        const anchor = document.createElement('a');
         const card = document.createElement('div');
         const img = document.createElement('img');
         const header = document.createElement('h3');
@@ -23,8 +37,18 @@ const displayData = (data) => {
         const para2 = document.createElement('p');
 
         card.className = "card"
-        header.className = "title"
-        para2.className = "price white"
+        anchor.className = "card_hover";
+        img.className = "card_img"
+         
+        if (obj.color == 'white') {
+            header.className = "title title-black";
+            para1.className = "description black"
+            para2.className = "price black"
+        } else {
+            header.className = "title title-white"
+            para1.className = "description white"
+            para2.className = "price white"
+        }
 
         img.src = obj.imgFront;
         header.textContent = obj.company;
@@ -33,9 +57,10 @@ const displayData = (data) => {
 
         console.log(para2);
 
+        products.appendChild(anchor);
+        anchor.appendChild(card)
         card.appendChild(img);
         card.appendChild(section);
-        products.appendChild(card);
         section.appendChild(header);
         section.appendChild(para1);
         section.appendChild(para2);
