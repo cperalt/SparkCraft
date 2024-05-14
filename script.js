2642034c6e1346dd5dc0bac182b5edea958e76ac
cperalt <cperalt@github.com>
2024-05-14 00:42:42 -0400

    mouseover & mouseout listeners

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-//targeting html elements
-let sortedData;
 
-
-//Add an event listener to hamburger menu that upon click toggles the navbar_links display, and also the overflow behavior
-//Also does extra styling for a better mobile experience
-
-let menu = document.querySelector("#navbar--hamburger");
-menu.addEventListener("click", () => {
-
-    let links = document.querySelector(".navbar_links");
-
-
-     if ( links.style.display === "flex") {
-         links.style.display = "none";
-         document.body.style.overflow = "auto";
-     } else {
-         links.style.display = "flex";
-         links.style.flexDirection = "column";
-         links.style.alignItems = "center";
-         document.body.style.overflow = "hidden";
-     }
-
-});
-
-
+//async function that fetches data
 async function fetchData() {
     try {
         const response = await fetch('./data.json');
         if (!response.ok) {
             throw new Error('Failed to fetch data');
         }
-        const data = await response.json()
-
-        sortData(data);
-        displayData(sortedData);
+    const data = await response.json()
+    return data;
     } catch (error) {
         console.error('Error:', error);
     }
 };
+
 
 const sortData = (data) => {
     data.sort((a, b) => {
@@ -52,9 +27,11 @@ const sortData = (data) => {
     let arr = [];
     let a = data.length - 1; 
     let b = a/2;
-    for(let i = 0; i < b; i++) arr.push( data[i], data[a - i]);
+    for(let i = 0; i < b; i++) {
+        arr.push( data[i], data[a - i])
+    };
 
-    sortedData = arr;
+    return arr;
 };
 
 const displayData = (data) => {
@@ -102,11 +79,70 @@ const displayData = (data) => {
         section.appendChild(header);
         section.appendChild(para1);
         section.appendChild(para2);
+
+        const mouseOver = () => {
+            header.textContent = obj.name;
+            para2.textContent = obj.history;
+            para1.style.display = "none";
+            // img.src = obj.imgBack;
+        }
+
+        const mouseOut = () => {
+            header.textContent = obj.company;
+            para1.textContent = obj.name;
+            para2.textContent = obj.price;
+            para1.style.display = "block";
+            // img.src = obj.imgFront;
+        }
+
+        anchor.addEventListener("mouseover", mouseOver)
+        anchor.addEventListener("mouseout", mouseOut)
         
     })
 }
 
-fetchData();
+// const mouseOver = () => {
+//     const anchor = document.querySelectorAll(".card_hover");
+//     const title = document.querySelectorAll(".title");
+//     const para = document.querySelectorAll(".decription");
+//     const para2 = document.querySelectorAll(".price");
+
+//     title.
+// }
+
+
+const main = async () => {
+    const data = await fetchData();
+
+    const sortedData = sortData(data);
+    displayData(sortedData);
+}
+
+main();
+
+
+//Add an event listener to hamburger menu that upon click toggles the navbar_links display, and also the overflow behavior
+//Also does extra styling for a better mobile experience
+
+let menu = document.querySelector("#navbar--hamburger");
+menu.addEventListener("click", () => {
+
+    let links = document.querySelector(".navbar_links");
+
+
+     if ( links.style.display === "flex") {
+         links.style.display = "none";
+         document.body.style.overflow = "auto";
+     } else {
+         links.style.display = "flex";
+         links.style.flexDirection = "column";
+         links.style.alignItems = "center";
+         document.body.style.overflow = "hidden";
+     }
+
+});
+
+
 
 // const displayHomeCards = ((data) => {
 
@@ -122,3 +158,26 @@ fetchData();
 
 
 // })
+
+const paymentNumbers = document.querySelectorAll(".num_field");
+
+paymentNumbers.forEach((btn) => {
+    btn.addEventListener("keydown", () => {
+
+         // Regrex checks for non numerical characters, if no match it will return -1 which means all characters are numbers
+         // I set the condition to check if its not -1 meaning non numerical numbers were typed. I run an alert to notify
+         // the user to enter only numbers.
+        if ( event.key.search(/\D/g) != -1 && event.key !== "Backspace" && event.key !== "Enter") {
+            window.alert("PLEASE ENTER ONLY NUMBERS");
+
+            //prevents the key from being pressed/added to the cardnumber.value
+            event. preventDefault()
+
+        }
+    })
+});
+
+
+const cardNumber = document.querySelector("#cardnumber");
+
+console.log(cardNumber);
